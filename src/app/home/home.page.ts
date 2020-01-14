1e3f6e2627eef41aa59db3437ae76c994d06fffe
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-01-13 16:21:08 -0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  imgUrls = [
+    'https://placekitten.com/500/600',
+    'https://placekitten.com/400/500',
+    'https://placekitten.com/600/700',
+    'https://placekitten.com/500/700',
+    'https://placekitten.com/700/800',
+    'https://placekitten.com/800/900',
+    'https://placekitten.com/300/400',
+    'https://placekitten.com/600/700',
+    'https://placekitten.com/500/700',
+    'https://placekitten.com/700/800',
+    'https://placekitten.com/50/50',
+  ]
+
   constructor() {}
 
 }
